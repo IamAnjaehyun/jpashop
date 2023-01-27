f9f45980b4cbc01cfec89ba8bf7e26884decf96d
Anjaehyun <lvet0330@gmail.com>
2023-01-27 16:33:44 +0900

    # 도메인 분석 설계
    domain 생성

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+@Embeddable
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //JPA 위해 그냥 만들기만 해놓음, 쓰라고 만든건 아니여서 protected
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //이건 꼭 STRING으로 써야됨. ORI~ 쓰면 안됨
+    private DeliveryStatus status; //READY, COMP
+}
