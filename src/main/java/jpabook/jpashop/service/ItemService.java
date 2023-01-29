9eb0198c14060309d7b5ee20acc9fc3fe58de58d
Anjaehyun <lvet0330@gmail.com>
2023-01-29 14:07:18 +0900

    # 상품 도메인 개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //저장안됨 롤백됨
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional//얘는 저장되어야함
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
